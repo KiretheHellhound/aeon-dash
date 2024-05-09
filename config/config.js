66dc6f7cb7d6d7f2c100f4bc785254f92eb91c41
KiretheHellhound <134233666+KiretheHellhound@users.noreply.github.com>
2024-05-08 22:44:46 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 let config = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5432,
     db: {
         uri: process.env.DATABASE_URL || 'postgres://udqe3kfp1jh822:p40af568c59660148be4187d7ba0826989410a5ced025013637b9736019ea3acf@ce1r1ldap2qd4b.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dd2g54jpq3dr6t',
         options: {
