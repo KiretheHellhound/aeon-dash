2e1105802b7b541ee3889882eeaacb7166925231
KiretheHellhound <134233666+KiretheHellhound@users.noreply.github.com>
2024-05-08 22:26:56 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 let config = {
     port: process.env.PORT || 3000,
     db: {
-        uri: process.env.DATABASE_URL || 'postgres://postgres:heslo@localhost:5432/torquedash',
+        uri: process.env.DATABASE_URL || 'postgres://udqe3kfp1jh822:p40af568c59660148be4187d7ba0826989410a5ced025013637b9736019ea3acf@ce1r1ldap2qd4b.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dd2g54jpq3dr6t',
         options: {
             logging: false
         }
     },
     session: {
-        keys: process.env.SESSION_KEYS || ['6a5w4d65a4wd', 'a65w4d6aw4d89a4', '65f4b8b4szd8']
+        keys: process.env.SESSION_KEYS || ['dd2g54jpq3dr6t', 'udqe3kfp1jh822']
     }
 };
 
